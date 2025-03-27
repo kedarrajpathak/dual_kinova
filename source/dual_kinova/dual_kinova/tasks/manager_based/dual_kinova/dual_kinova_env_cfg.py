4c2d31898140ce3b3b90b2b6b41c4bb54e00b3de
Kedar <kedar.rajpathak@rwth-aachen.de>
2025-03-27 17:55:20 +0100

    env edit

--- source/dual_kinova/dual_kinova/tasks/manager_based/dual_kinova/dual_kinova_env_cfg.py
+++ source/dual_kinova/dual_kinova/tasks/manager_based/dual_kinova/dual_kinova_env_cfg.py
@@ -3,11 +3,16 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+import os
+import torch
+from torchvision.utils import save_image
 import math
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg, AssetBaseCfg
-from isaaclab.envs import ManagerBasedRLEnvCfg
+import isaaclab.utils.math as math_utils
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg, RigidObject
+from isaaclab.sensors import Camera, CameraCfg, RayCasterCamera, TiledCameraCfg, TiledCamera
+from isaaclab.envs import ManagerBasedRLEnvCfg, ManagerBasedEnv
 from isaaclab.managers import EventTermCfg as EventTerm
 from isaaclab.managers import ObservationGroupCfg as ObsGroup
 from isaaclab.managers import ObservationTermCfg as ObsTerm
@@ -17,13 +22,82 @@ from isaaclab.managers import TerminationTermCfg as DoneTerm
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.utils import configclass
 
-from . import mdp
+from isaaclab_tasks.manager_based.manipulation.lift import mdp
 
 ##
 # Pre-defined configs
 ##
 
-from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
+from isaaclab_assets.robots.kinova import DUAL_KINOVA_CFG  # isort:skip
+
+
+def image(
+    env: ManagerBasedEnv,
+    sensor_cfg: SceneEntityCfg = SceneEntityCfg("tiled_camera"),
+    data_type: str = "rgb",
+    convert_perspective_to_orthogonal: bool = False,
+    normalize: bool = True,
+    save_image_to_file: bool = False,
+    image_path: str = "image",
+) -> torch.Tensor:
+    """Images of a specific datatype from the camera sensor.
+
+    If the flag :attr:`normalize` is True, post-processing of the images are performed based on their
+    data-types:
+
+    - "rgb": Scales the image to (0, 1) and subtracts with the mean of the current image batch.
+    - "depth" or "distance_to_camera" or "distance_to_plane": Replaces infinity values with zero.
+
+    Args:
+        env: The environment the cameras are placed within.
+        sensor_cfg: The desired sensor to read from. Defaults to SceneEntityCfg("tiled_camera").
+        data_type: The data type to pull from the desired camera. Defaults to "rgb".
+        convert_perspective_to_orthogonal: Whether to orthogonalize perspective depth images.
+            This is used only when the data type is "distance_to_camera". Defaults to False.
+        normalize: Whether to normalize the images. This depends on the selected data type.
+            Defaults to True.
+
+    Returns:
+        The images produced at the last time-step
+    """
+    # extract the used quantities (to enable type-hinting)
+    sensor: TiledCamera | Camera | RayCasterCamera = env.scene.sensors[sensor_cfg.name]
+
+    # obtain the input image
+    images = sensor.data.output[data_type]
+
+    # depth image conversion
+    if (data_type == "distance_to_camera") and convert_perspective_to_orthogonal:
+        images = math_utils.orthogonalize_perspective_depth(images, sensor.data.intrinsic_matrices)
+
+    # rgb/depth image normalization
+    if normalize:
+        if data_type == "rgb":
+            images = images.float() / 255.0
+            mean_tensor = torch.mean(images, dim=(1, 2), keepdim=True)
+            images -= mean_tensor
+        elif "distance_to" in data_type or "depth" in data_type:
+            images[images == float("inf")] = 0
+        elif data_type == "normals":
+            images = (images + 1.0) * 0.5
+
+    if save_image_to_file:
+        dir_path, _ = os.path.split(image_path)
+        if dir_path:
+            os.makedirs(dir_path, exist_ok=True)
+        if images.dtype == torch.uint8:
+            images = images.float() / 255.0
+        # Get total successful episodes
+        total_successes = sum(env.recorder_manager._exported_successful_episode_count.values())
+
+        for tile in range(images.shape[0]):
+            tile_chw = torch.swapaxes(images[tile : tile + 1].unsqueeze(1), 1, -1).squeeze(-1)
+            filename = (
+                f"{image_path}_{data_type}_trial_{total_successes}_tile_{tile}_step_{env.common_step_counter}.png"
+            )
+            save_image(tile_chw, filename)
+
+    return images.clone()
 
 
 ##
@@ -35,22 +109,49 @@ from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
 class DualKinovaSceneCfg(InteractiveSceneCfg):
     """Configuration for a cart-pole scene."""
 
-    # ground plane
-    ground = AssetBaseCfg(
-        prim_path="/World/ground",
-        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
-    )
-
     # robot
-    robot: ArticulationCfg = CARTPOLE_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-
+    robot: ArticulationCfg = DUAL_KINOVA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    
+    # RGB-D camera
+    tiled_camera = TiledCameraCfg(
+        prim_path="{ENV_REGEX_NS}/Robot",
+        update_period=0.1,
+        height=480,
+        width=640,
+        data_types=["rgb", "distance_to_image_plane"],
+        spawn=None,  # the camera is already spawned in the scene
+        offset=TiledCameraCfg.OffsetCfg(pos=(0.0, 0.0, 0.5), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
+    )
+    camera = CameraCfg(
+        prim_path="{ENV_REGEX_NS}/Robot/base/front_cam",
+        update_period=0.1,
+        height=480,
+        width=640,
+        data_types=["rgb", "distance_to_image_plane"],
+        spawn=sim_utils.PinholeCameraCfg(
+            focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 1.0e5)
+        ),
+        offset=CameraCfg.OffsetCfg(pos=(0.0, 0.0, 0.5), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
+    )
+    
     # lights
     dome_light = AssetBaseCfg(
         prim_path="/World/DomeLight",
         spawn=sim_utils.DomeLightCfg(color=(0.9, 0.9, 0.9), intensity=500.0),
     )
 
-
+    # cube
+    cube = RigidObjectCfg(
+        prim_path="{ENV_REGEX_NS}/Cube",
+        init_state=RigidObjectCfg.InitialStateCfg(pos=(0.5, 1.0, 0.1), rot=(1.0, 0.0, 0.0, 0.0)),
+        spawn=sim_utils.CuboidCfg(
+            size=(0.1, 0.1, 0.1),
+            rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+            mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+            collision_props=sim_utils.CollisionPropertiesCfg(),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0), metallic=0.2),   
+        )
+    )
 ##
 # MDP settings
 ##
@@ -62,6 +163,15 @@ class ActionsCfg:
 
     joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["slider_to_cart"], scale=100.0)
 
+        # Set actions for the specific robot type (franka)
+    arm_action = DifferentialInverseKinematicsActionCfg(
+        asset_name="robot",
+        joint_names=["panda_joint.*"],
+        body_name="panda_hand",
+        controller=DifferentialIKControllerCfg(command_type="pose", use_relative_mode=True, ik_method="dls"),
+        scale=0.5,
+        body_offset=DifferentialInverseKinematicsActionCfg.OffsetCfg(pos=[0.0, 0.0, 0.107]),
+    )
 
 @configclass
 class ObservationsCfg:
@@ -69,19 +179,55 @@ class ObservationsCfg:
 
     @configclass
     class PolicyCfg(ObsGroup):
-        """Observations for policy group."""
+        """Observations for policy group with state values."""
 
-        # observation terms (order preserved)
-        joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
-        joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
+        actions = ObsTerm(func=mdp.last_action)
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel)
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
+        object = ObsTerm(func=mdp.instance_randomize_object_obs)
+        cube_positions = ObsTerm(func=mdp.instance_randomize_cube_positions_in_world_frame)
+        cube_orientations = ObsTerm(func=mdp.instance_randomize_cube_orientations_in_world_frame)
+        eef_pos = ObsTerm(func=mdp.ee_frame_pos)
+        eef_quat = ObsTerm(func=mdp.ee_frame_quat)
+        gripper_pos = ObsTerm(func=mdp.gripper_pos)
 
-        def __post_init__(self) -> None:
+        def __post_init__(self):
             self.enable_corruption = False
-            self.concatenate_terms = True
+            self.concatenate_terms = False
+
+    @configclass
+    class RGBCameraPolicyCfg(ObsGroup):
+        """Observations for policy group with RGB images."""
+
+
+        rgb = ObsTerm(
+            func=image,
+            params={
+                "sensor_cfg": SceneEntityCfg("camera"),
+                "data_type": "rgb",
+                "normalize": True,
+                "save_image_to_file": True,
+                "image_path": "rgb",
+            },
+        )
+        depth = ObsTerm(
+            func=image,
+            params={
+                "sensor_cfg": SceneEntityCfg("camera"),
+                "data_type": "distance_to_image_plane",
+                "normalize": True,
+                "save_image_to_file": True,
+                "image_path": "depth",
+            },
+        )
+
+        def __post_init__(self):
+            self.enable_corruption = False
+            self.concatenate_terms = False
 
     # observation groups
     policy: PolicyCfg = PolicyCfg()
-
+    rgb_camera: RGBCameraPolicyCfg = RGBCameraPolicyCfg()
 
 @configclass
 class EventCfg:
