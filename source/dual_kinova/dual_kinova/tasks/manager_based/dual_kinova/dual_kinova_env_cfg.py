d12da28ab9decf09c5358acc213232ede9a56c9f
Kedar <kedar.rajpathak@rwth-aachen.de>
2025-03-28 15:07:41 +0100

    rl env works

--- source/dual_kinova/dual_kinova/tasks/manager_based/dual_kinova/dual_kinova_env_cfg.py
+++ source/dual_kinova/dual_kinova/tasks/manager_based/dual_kinova/dual_kinova_env_cfg.py
@@ -12,7 +12,9 @@ import isaaclab.sim as sim_utils
 import isaaclab.utils.math as math_utils
 from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg, RigidObject
 from isaaclab.sensors import Camera, CameraCfg, RayCasterCamera, TiledCameraCfg, TiledCamera
-from isaaclab.envs import ManagerBasedRLEnvCfg, ManagerBasedEnv
+from isaaclab.controllers.differential_ik_cfg import DifferentialIKControllerCfg
+from isaaclab.envs.mdp.actions.actions_cfg import DifferentialInverseKinematicsActionCfg
+from isaaclab.envs import ManagerBasedRLEnvCfg, ManagerBasedRLEnv
 from isaaclab.managers import EventTermCfg as EventTerm
 from isaaclab.managers import ObservationGroupCfg as ObsGroup
 from isaaclab.managers import ObservationTermCfg as ObsTerm
@@ -30,76 +32,6 @@ from isaaclab_tasks.manager_based.manipulation.lift import mdp
 
 from isaaclab_assets.robots.kinova import DUAL_KINOVA_CFG  # isort:skip
 
-
-def image(
-    env: ManagerBasedEnv,
-    sensor_cfg: SceneEntityCfg = SceneEntityCfg("tiled_camera"),
-    data_type: str = "rgb",
-    convert_perspective_to_orthogonal: bool = False,
-    normalize: bool = True,
-    save_image_to_file: bool = False,
-    image_path: str = "image",
-) -> torch.Tensor:
-    """Images of a specific datatype from the camera sensor.
-
-    If the flag :attr:`normalize` is True, post-processing of the images are performed based on their
-    data-types:
-
-    - "rgb": Scales the image to (0, 1) and subtracts with the mean of the current image batch.
-    - "depth" or "distance_to_camera" or "distance_to_plane": Replaces infinity values with zero.
-
-    Args:
-        env: The environment the cameras are placed within.
-        sensor_cfg: The desired sensor to read from. Defaults to SceneEntityCfg("tiled_camera").
-        data_type: The data type to pull from the desired camera. Defaults to "rgb".
-        convert_perspective_to_orthogonal: Whether to orthogonalize perspective depth images.
-            This is used only when the data type is "distance_to_camera". Defaults to False.
-        normalize: Whether to normalize the images. This depends on the selected data type.
-            Defaults to True.
-
-    Returns:
-        The images produced at the last time-step
-    """
-    # extract the used quantities (to enable type-hinting)
-    sensor: TiledCamera | Camera | RayCasterCamera = env.scene.sensors[sensor_cfg.name]
-
-    # obtain the input image
-    images = sensor.data.output[data_type]
-
-    # depth image conversion
-    if (data_type == "distance_to_camera") and convert_perspective_to_orthogonal:
-        images = math_utils.orthogonalize_perspective_depth(images, sensor.data.intrinsic_matrices)
-
-    # rgb/depth image normalization
-    if normalize:
-        if data_type == "rgb":
-            images = images.float() / 255.0
-            mean_tensor = torch.mean(images, dim=(1, 2), keepdim=True)
-            images -= mean_tensor
-        elif "distance_to" in data_type or "depth" in data_type:
-            images[images == float("inf")] = 0
-        elif data_type == "normals":
-            images = (images + 1.0) * 0.5
-
-    if save_image_to_file:
-        dir_path, _ = os.path.split(image_path)
-        if dir_path:
-            os.makedirs(dir_path, exist_ok=True)
-        if images.dtype == torch.uint8:
-            images = images.float() / 255.0
-        # Get total successful episodes
-        total_successes = sum(env.recorder_manager._exported_successful_episode_count.values())
-
-        for tile in range(images.shape[0]):
-            tile_chw = torch.swapaxes(images[tile : tile + 1].unsqueeze(1), 1, -1).squeeze(-1)
-            filename = (
-                f"{image_path}_{data_type}_trial_{total_successes}_tile_{tile}_step_{env.common_step_counter}.png"
-            )
-            save_image(tile_chw, filename)
-
-    return images.clone()
-
-
 ##
 # Scene definition
 ##
@@ -111,19 +43,9 @@ class DualKinovaSceneCfg(InteractiveSceneCfg):
 
     # robot
     robot: ArticulationCfg = DUAL_KINOVA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-    
-    # RGB-D camera
-    tiled_camera = TiledCameraCfg(
-        prim_path="{ENV_REGEX_NS}/Robot",
-        update_period=0.1,
-        height=480,
-        width=640,
-        data_types=["rgb", "distance_to_image_plane"],
-        spawn=None,  # the camera is already spawned in the scene
-        offset=TiledCameraCfg.OffsetCfg(pos=(0.0, 0.0, 0.5), rot=(0.5, -0.5, 0.5, -0.5), convention="ros"),
-    )
+
     camera = CameraCfg(
-        prim_path="{ENV_REGEX_NS}/Robot/base/front_cam",
+        prim_path="{ENV_REGEX_NS}/cam",
         update_period=0.1,
         height=480,
         width=640,
@@ -161,17 +83,35 @@ class DualKinovaSceneCfg(InteractiveSceneCfg):
 class ActionsCfg:
     """Action specifications for the MDP."""
 
-    joint_effort = mdp.JointEffortActionCfg(asset_name="robot", joint_names=["slider_to_cart"], scale=100.0)
-
         # Set actions for the specific robot type (franka)
-    arm_action = DifferentialInverseKinematicsActionCfg(
+    left_arm = DifferentialInverseKinematicsActionCfg(
         asset_name="robot",
-        joint_names=["panda_joint.*"],
-        body_name="panda_hand",
-        controller=DifferentialIKControllerCfg(command_type="pose", use_relative_mode=True, ik_method="dls"),
+        joint_names=["left_joint_.*"],
+        body_name="left_end_effector_link",
+        controller=DifferentialIKControllerCfg(command_type="pose", use_relative_mode=False, ik_method="dls"),
         scale=0.5,
-        body_offset=DifferentialInverseKinematicsActionCfg.OffsetCfg(pos=[0.0, 0.0, 0.107]),
+        body_offset=DifferentialInverseKinematicsActionCfg.OffsetCfg(pos=(0.0, 0.0, 0.15)),
     )
+    left_gripper = mdp.BinaryJointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["left_robotiq_85_left_knuckle_joint"],
+            open_command_expr={"left_robotiq_85_left_knuckle_joint": 0.78},
+            close_command_expr={"left_robotiq_85_left_knuckle_joint": 0.0},
+        )
+    right_arm = DifferentialInverseKinematicsActionCfg(
+        asset_name="robot",
+        joint_names=["right_joint_.*"],
+        body_name="right_end_effector_link",
+        controller=DifferentialIKControllerCfg(command_type="pose", use_relative_mode=False, ik_method="dls"),
+        scale=0.5,
+        body_offset=DifferentialInverseKinematicsActionCfg.OffsetCfg(pos=(0.0, 0.0, 0.15)),
+    )
+    right_gripper = mdp.BinaryJointPositionActionCfg(
+            asset_name="robot",
+            joint_names=["right_robotiq_85_left_knuckle_joint"],
+            open_command_expr={"right_robotiq_85_left_knuckle_joint": 0.78},
+            close_command_expr={"right_robotiq_85_left_knuckle_joint": 0.0},
+        )
 
 @configclass
 class ObservationsCfg:
@@ -182,14 +122,10 @@ class ObservationsCfg:
         """Observations for policy group with state values."""
 
         actions = ObsTerm(func=mdp.last_action)
-        joint_pos = ObsTerm(func=mdp.joint_pos_rel)
-        joint_vel = ObsTerm(func=mdp.joint_vel_rel)
-        object = ObsTerm(func=mdp.instance_randomize_object_obs)
-        cube_positions = ObsTerm(func=mdp.instance_randomize_cube_positions_in_world_frame)
-        cube_orientations = ObsTerm(func=mdp.instance_randomize_cube_orientations_in_world_frame)
-        eef_pos = ObsTerm(func=mdp.ee_frame_pos)
-        eef_quat = ObsTerm(func=mdp.ee_frame_quat)
-        gripper_pos = ObsTerm(func=mdp.gripper_pos)
+        joint_pos = ObsTerm(func=mdp.joint_pos)
+        joint_vel = ObsTerm(func=mdp.joint_vel)
+        joint_pos_rel = ObsTerm(func=mdp.joint_pos_rel)
+        joint_vel_rel = ObsTerm(func=mdp.joint_vel_rel)
 
         def __post_init__(self):
             self.enable_corruption = False
@@ -199,25 +135,21 @@ class ObservationsCfg:
     class RGBCameraPolicyCfg(ObsGroup):
         """Observations for policy group with RGB images."""
 
-
         rgb = ObsTerm(
-            func=image,
+            func=mdp.image,
             params={
                 "sensor_cfg": SceneEntityCfg("camera"),
                 "data_type": "rgb",
                 "normalize": True,
-                "save_image_to_file": True,
-                "image_path": "rgb",
             },
         )
+        
         depth = ObsTerm(
-            func=image,
+            func=mdp.image,
             params={
                 "sensor_cfg": SceneEntityCfg("camera"),
                 "data_type": "distance_to_image_plane",
                 "normalize": True,
-                "save_image_to_file": True,
-                "image_path": "depth",
             },
         )
 
@@ -227,32 +159,14 @@ class ObservationsCfg:
 
     # observation groups
     policy: PolicyCfg = PolicyCfg()
-    rgb_camera: RGBCameraPolicyCfg = RGBCameraPolicyCfg()
+    rgbd_camera: RGBCameraPolicyCfg = RGBCameraPolicyCfg()
 
 @configclass
 class EventCfg:
     """Configuration for events."""
 
     # reset
-    reset_cart_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]),
-            "position_range": (-1.0, 1.0),
-            "velocity_range": (-0.5, 0.5),
-        },
-    )
-
-    reset_pole_position = EventTerm(
-        func=mdp.reset_joints_by_offset,
-        mode="reset",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]),
-            "position_range": (-0.25 * math.pi, 0.25 * math.pi),
-            "velocity_range": (-0.25 * math.pi, 0.25 * math.pi),
-        },
-    )
+    reset_scene = EventTerm(func=mdp.reset_scene_to_default, mode="reset")
 
 
 @configclass
@@ -263,24 +177,24 @@ class RewardsCfg:
     alive = RewTerm(func=mdp.is_alive, weight=1.0)
     # (2) Failure penalty
     terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
-    # (3) Primary task: keep pole upright
-    pole_pos = RewTerm(
-        func=mdp.joint_pos_target_l2,
-        weight=-1.0,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
-    )
-    # (4) Shaping tasks: lower cart velocity
-    cart_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.01,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
-    )
-    # (5) Shaping tasks: lower pole angular velocity
-    pole_vel = RewTerm(
-        func=mdp.joint_vel_l1,
-        weight=-0.005,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
-    )
+    # # (3) Primary task: keep pole upright
+    # pole_pos = RewTerm(
+    #     func=mdp.joint_pos_target_l2,
+    #     weight=-1.0,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"]), "target": 0.0},
+    # )
+    # # (4) Shaping tasks: lower cart velocity
+    # cart_vel = RewTerm(
+    #     func=mdp.joint_vel_l1,
+    #     weight=-0.01,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"])},
+    # )
+    # # (5) Shaping tasks: lower pole angular velocity
+    # pole_vel = RewTerm(
+    #     func=mdp.joint_vel_l1,
+    #     weight=-0.005,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["cart_to_pole"])},
+    # )
 
 
 @configclass
@@ -290,11 +204,16 @@ class TerminationsCfg:
     # (1) Time out
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
     # (2) Cart out of bounds
-    cart_out_of_bounds = DoneTerm(
-        func=mdp.joint_pos_out_of_manual_limit,
-        params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
+    # cart_out_of_bounds = DoneTerm(
+    #     func=mdp.joint_pos_out_of_manual_limit,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*"]), "bounds": (-3.0, 3.0)},
+    # )
+    # (3) Velocity out of bounds
+    velocity_out_of_bounds = DoneTerm(
+        func=mdp.joint_vel_out_of_limit,
+        params={"asset_cfg": SceneEntityCfg("robot", joint_names=[".*"])}
     )
-
+    
 
 ##
 # Environment configuration
@@ -302,7 +221,7 @@ class TerminationsCfg:
 
 
 @configclass
-class DualKinovaEnvCfg(ManagerBasedRLEnvCfg):
+class DualKinovaRLEnvCfg(ManagerBasedRLEnvCfg):
     # Scene settings
     scene: DualKinovaSceneCfg = DualKinovaSceneCfg(num_envs=4096, env_spacing=4.0)
     # Basic settings
@@ -318,7 +237,7 @@ class DualKinovaEnvCfg(ManagerBasedRLEnvCfg):
         """Post initialization."""
         # general settings
         self.decimation = 2
-        self.episode_length_s = 5
+        self.episode_length_s = 10
         # viewer settings
         self.viewer.eye = (8.0, 0.0, 5.0)
         # simulation settings
